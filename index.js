84cb445d483313d62f404889bc3baec076af86ec
remove <b508099021@tmu.edu.tw>
2022-12-21 18:34:06 +0800

    [Add] all turf features and create a polygon instance.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-// import turfHelpers from '@turf/helpers';
+// require 寫法
 const turfHelpers = require('@turf/helpers');
+const turf = require('@turf/turf');
 
-const polygon1 = turfHelpers.polygon(
+// import turfHelpers, { polygon } from '@turf/helpers';
+// import turf from '@turf/turf';
+
+// data definition zone
+const dataPolygon = [
   [
-    [
-      [-10, 52],
-      [-4, 56],
-      [0, 51],
-      [-5, 26],
-      [-10, 52],
-    ],
+    [-10, 52],
+    [-4, 56],
+    [0, 51],
+    [-5, 26],
+    [-10, 52],
   ],
-  { name: 'poly1' }
-);
-console.log('turfHelpers: ', turfHelpers);
+];
+
+const polygon1 = turfHelpers.polygon(dataPolygon, { name: 'poly1' });
+
+const polygonInstance = turf.polygon(dataPolygon);
+const polygonCenterOfMass = turf.centerOfMass(polygonInstance);
+
+// console.log('turf: ', turf);
+// console.log('turfHelpers: ', turfHelpers);
 console.log('polygon1: ', polygon1);
+console.log('polygonInstance: ', polygonInstance);
+console.log('polygonCenterOfMass: ', polygonCenterOfMass);
